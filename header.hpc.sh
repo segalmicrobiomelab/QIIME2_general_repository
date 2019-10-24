6be7d89760fa014e966732d06e4c89d43e693477
Benjamin Wu <wub02@nyumc.org>
2019-10-24 17:10:56 -0400

    all.files

---
+++ header.hpc.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SBATCH --job-name=MSQ106# Job name
+#SBATCH --mail-type=BEGIN,END,FAIL      	 # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=benjamin.wu@nyumc.org        # Where to send mail
+#SBATCH --ntasks=16                   		 # Run on a single CPU
+#SBATCH --mem=64gb                    		 # Job memory request
+#SBATCH --time=72:05:00             		 # Time limit hrs:min:sec
+#SBATCH --output=serial_test_%j.log   # Standard output and error log
