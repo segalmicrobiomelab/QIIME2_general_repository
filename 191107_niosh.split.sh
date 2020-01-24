d314243a024427434281eec77174d9110cd349ca
Benjamin Wu <benjaminwu@bananaspajamas2.bhc.med.nyu.edu>
2020-01-24 12:25:23 -0500

    aspera

--- 191107_niosh.split.sh
+++ 191107_niosh.split.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks=16                   		 # Run on a single CPU
 #SBATCH --mem=64gb                    		 # Job memory request
 #SBATCH --time=72:05:00             		 # Time limit hrs:min:sec
-#SBATCH --output=serial_test_%j.log              # Standard output and error log
+#SBATCH --output=serial_test_%j.txt              # Standard output and error log
 
 module add miniconda2/4.5.4
 conda info --env
